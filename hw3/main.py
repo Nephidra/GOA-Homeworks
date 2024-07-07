f0188f43459417872660b87eb74427dc9e584ae5
Nephidra <irakliugulava89@gmail.com>
2024-07-07 21:47:05 +0400

    a

---
+++ hw3/main.py
@@ -0,0 +1,14 @@
+print(5+5)
+# Mimateba
+print(15/5)
+# Gayofa
+print(7-3)
+# Gamokleba
+print(13*13)
+# Gamravleba
+
+x = 3
+y = 7
+# Cvladebis sheqmna
+print(x+y)
+# Cvladebis shekreba
---
+++ hw5/main.py
@@ -0,0 +1,34 @@
+name = "Irakli"
+surname = "Ugulava"
+age = 15
+adress = "Cotne Dadianis 314ze"
+# Created variables
+
+
+print("Me var", name, surname, "var", age, "wlis. vcxovrob", adress)
+# Info out
+
+int_1 = 12 
+int_2 = 26 
+int_3 = 31 
+int_4 = 45
+int_5 = 50 
+int_6 = 61 
+int_7 = 72 
+int_8 = 88
+int_9 = 94
+int_10 = 10 
+# Created variables
+
+math_1 = int_1 + int_2
+math_2 = int_2 * int_10
+math_3 = int_3 / int_2
+math_4 = int_4 - int_8
+math_5 = int_5 * int_1
+math_6 = int_6 + int_2
+math_7 = int_7 - int_10
+math_8 = int_8 * int_8
+math_9 = int_9 * int_5
+math_10 = int_10 + int_1
+
+# Did math
