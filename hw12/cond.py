50a83e9726729c155037352fa2b8c99d8a75481f
Nephidra <irakliugulava89@gmail.com>
2024-07-23 21:26:14 +0400

    done

---
+++ hw12/cond.py
@@ -0,0 +1,25 @@
+age = int(input("Input your age "))
+
+if age < 0 :
+    print("Uaryofiti")
+else :
+    print("Dadebiti")
+
+
+num = int(input("Input any number "))
+
+print(num == 10)
+
+age_1 = int(input("Input any number "))
+
+if age_1 > 0 and age_1 < 10 :
+    print("child")
+
+side_1 = int(input("First side"))
+side_2 = int(input("First second"))
+side_3 = int(input("First third"))
+
+if side_1 + side_2 > side_3 :
+    if side_2 + side_3 > side_1 :
+        if side_3 + side_1 > side_2 :
+            print("Shesadzlebelia")
---
+++ hw12/main.py
@@ -0,0 +1,18 @@
+# i = 1
+# for i in range(11) :
+#     if i != 0 :
+#         print(i)
+#     print("Hello world")
+
+# i_2 = 1
+
+# while i_2 < 6 :
+#     print(i_2)
+#     i_2 += 1
+
+i_3 = 0
+
+while i_3 < 3 : 
+    print("Hello world")
+    i_3 += 1
+
