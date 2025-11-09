2e3ccb9a34d67368dfec0f80f28a5d3ae5cc9e87
Irakli <irakliugulava89@gmail.com>
2025-11-09 13:18:39 +0400

    a

---
+++ Day-0140/hw/main.js
@@ -0,0 +1,36 @@
+// import chalk from 'chalk';
+// console.log(chalk.green('გამარჯობა!'));
+// console.log(chalk.red('შეცდომა!'));
+
+// import figlet from "figlet";
+
+// figlet("I r a k l i", (err, data) => {
+//   if (err) {
+//     console.log("Error:", err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+
+// const moment = require("moment");
+// console.log(moment().format("YYYY-MM-DD HH:mm"));
+
+
+// import inquirer from "inquirer";
+
+// const answers = await inquirer.prompt([
+//   {
+//     type: "input",
+//     name: "username",
+//     message: "your name:",
+//   },
+// ]);
+
+// console.log(`hello, ${answers.username}!`);
+
+// import { v4 as uuidv4 } from "uuid";
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(uuidv4());
+// }
